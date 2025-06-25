f8ff5e4f97f16ea13b32593c62b1b5aa6d6b364d
Mike Linford <mike.linford@gmail.com>
2025-06-25 17:20:23 -0400

    v0.0.1 - added clap, thiserror, and impl-cli-main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+use impl_cli_main::impl_cli_main;
+
+impl_cli_main!();
+
+fn run(args: &CliArgs) -> Result<(), RunError> {
+    match args {
+        CliArgs {} => Ok(()),
+    }
 }
+
+#[derive(Parser)]
+struct CliArgs {}
+
+#[derive(Debug, thiserror::Error)]
+enum RunError {}
